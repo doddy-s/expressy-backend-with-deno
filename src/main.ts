88f2c1b94e351ce35512bae57a0c31e84bc5a2ce
doddy-s <doddy.s@protonmail.com>
2023-03-23 08:40:31 +0700

    Adds types for intellisense and adds body parsing

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,36 @@
-import express, { Request, Response } from "npm:express";
+// @deno-types="npm:@types/express"
+import express, { Request, Response,  } from "npm:express";
 const app = express();
+app.use(express.json());
 
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 const { PORT } = config();
-console.log(PORT);
 
 import signJwt from "./utils/jwtSigner.ts";
 import verifyJwt from "./utils/jwtVerifier.ts";
 
-app.get("/sign", async (req: Request, res: Response) => {
-  const name = req.headers["name"];
-  console.log(req.headers);
-  const signedJwt = await signJwt(name);
+app.post("/sign", async (req: Request, res: Response) => {
+  const username = req.body("username");
+  console.log(req.header);
+  const signedJwt = await signJwt(username);
   res.status(200).json({
     jwt: signedJwt,
   });
 });
 
 app.get("/verify", (req: Request, res: Response) => {
-  const token = req.headers["jwt"];
-  res.send(verifyJwt(token));
+  const token = req.header("token") as string;
+  verifyJwt(token);
+  res.status(200).json({
+    message: "verified",
+  });
 });
 
-app.get("/", (res: Response) => {
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
   res.status(200).json({
     hello: "hello",
   });
 });
 
-app.listen(PORT);
+app.listen(PORT, () => console.log("Listening on port " + PORT));
